ec416477ef4e4b2272bf5f35b702ace68401ac0e
jacobbleser <jacobbleser@aol.com>
2016-09-21 09:49:25 -0400

    Added html

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
-  res.end("yes") // <--- Edit this text to change whether late
-});
+app.use('/', express.static('public'));
 
 app.listen(process.env.PORT || 3000);
